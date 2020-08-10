56e45c24283c5bb6fcfdcc16a83197ab91b4f0d8
github username <github email address>
2020-08-10 14:42:19 -0400

    blooger_controller

---
+++ app/controllers/blogger_controller.rb
@@ -0,0 +1,37 @@
+class BloggerController < ApplicationController
+  
+  def index
+    @bloggers = Blogger.all
+  end
+
+  def show
+      find_blogger
+  end
+
+  def new
+    @blogger = Blogger.new
+  end
+
+  def create
+    @blogger = Blogger.create(blogger_params)
+
+    if @blogger.valid?
+      redirect_to blogger_path(@blogger)
+    else
+      flash[:notice] = @blogger.errors.full_messages
+      redirect_to new_blogger_path
+    end
+  end
+
+
+
+  private
+
+  def find_blogger
+    @blogger =  Blogger.find(params[:id])
+  end
+
+  def blogger_params
+    params.require(:blogger).permit(:name , :bio, :age)
+  end
+end
